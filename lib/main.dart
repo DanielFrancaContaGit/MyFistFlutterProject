48ae0bc60fe0af34ced89dc769d7265ca10c4591
Daniel <daniel.pe.15y15@gmail.com>
2020-05-02 23:09:52 -0300

    larning row and column

--- lib/main.dart
+++ lib/main.dart
@@ -13,19 +13,29 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.lightGreenAccent[700],
       ),
-      body: Center(
-        child: Text(
-          'oi mundo',
-          style: TextStyle(
-            fontSize: 30,
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Text('oi mundo'),
+          FlatButton(
+            onPressed: () {},
+            color: Colors.amber,
+            child: Text('me aperta'),
           ),
-        ),
+          Container(
+            color: Colors.cyan,
+            padding: EdgeInsets.all(20),
+            child: Text('inside container'),
+          )
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
-        child: Text('click'),
+        child: Icon(Icons.thumb_up),
         backgroundColor: Colors.lightGreenAccent[700],
       ),
+      
     );
   }
 }
