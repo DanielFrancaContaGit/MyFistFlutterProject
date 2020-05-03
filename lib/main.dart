bfe59f772414a850b051dba91c445fbf756f6e11
Daniel <daniel.pe.15y15@gmail.com>
2020-05-03 18:25:24 -0300

    app do pikachu

--- lib/main.dart
+++ lib/main.dart
@@ -15,26 +15,63 @@ class _NinjaCardState extends State<NinjaCard> {
 
   int _nijaLevel = 0;
 
+  String squadMaisLindo = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[900],
       appBar: AppBar(
-       title: Text('Ninja ID card'), 
+       title: Text('App do pikachu'), 
        centerTitle: true,
        backgroundColor: Colors.grey[850],
        elevation: 0.0,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            _nijaLevel++;
-          });
-        },
-        child: Icon(
-          Icons.add
-        ),
-        backgroundColor: Colors.grey[800],
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+         CircularButton(
+           height: 60,
+           width: 60,
+           color: Colors.green,
+           icon: Icon(
+             Icons.add
+           ),
+           onClick: () {
+             setState(() {
+               _nijaLevel++;
+             });
+           },
+         ),
+         CircularButton(
+           height: 60,
+           width: 60,
+           color: Colors.red,
+           icon: Icon(
+             Icons.remove
+           ),
+           onClick: () {
+             setState(() {
+               _nijaLevel--;
+             });
+           },
+         ),
+         FlatButton(
+           onPressed: () {
+             setState(() {
+               if ( squadMaisLindo == 'O squad de Edu'){
+                 squadMaisLindo = '';
+               } else {
+               squadMaisLindo = 'O squad de Edu';
+               }
+             });
+           }, 
+           color: Colors.amber,
+           child: Text('Qual é o squad mais lindo'),
+          ),
+        ],
       ),
       body: Padding(
         padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
@@ -70,7 +107,7 @@ class _NinjaCardState extends State<NinjaCard> {
             ),
             SizedBox(height: 30.0,),
             Text(
-              'CURRENT NINJA LEVEL',
+              'CURRENT PIKACHU LEVEL',
               style: TextStyle(
                 color: Colors.grey,
                 letterSpacing: 2.0,
@@ -93,7 +130,7 @@ class _NinjaCardState extends State<NinjaCard> {
                   Icons.email,
                   color: Colors.grey[400],
                 ),
-                SizedBox(width: 10.0,),
+                SizedBox(width: 10.0),
                 Text(
                   'botdopikachu@gmail.com',
                   style: TextStyle(
@@ -101,6 +138,19 @@ class _NinjaCardState extends State<NinjaCard> {
                     letterSpacing: 1.0,
                   ),
                 )
+              ],
+            ),
+            SizedBox(height: 30.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  '$squadMaisLindo',
+                  style: TextStyle(
+                    color: Colors.amber[700],
+                    fontSize: 30,
+                  ),
+                ),
               ],
             )
           ]
@@ -110,3 +160,24 @@ class _NinjaCardState extends State<NinjaCard> {
   }
 }
 
+
+class CircularButton extends StatelessWidget {
+
+  final double width;
+  final double height;
+  final Color color;
+  final Icon icon;
+  final Function onClick;
+
+  CircularButton({ this.color, this.height, this.icon, this.onClick, this.width });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(color: color, shape: BoxShape.circle),
+      width: width,
+      height: height,
+      child: IconButton(icon: icon, enableFeedback: true, onPressed: onClick),
+    );
+  }
+}
